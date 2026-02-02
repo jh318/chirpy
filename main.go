6406d89f02204ccba428f0a3e1c9d9687e6adf6f
jh318 <jhawley318@gmail.com>
2026-02-02 08:28:11 -0800

    Added logo file and path

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/assets/", http.StripPrefix("/assets/",http.FileServer(http.Dir("./assets"))))
 	server := http.Server{}
 	server.Handler = mux
 	server.Addr = ":8080"
